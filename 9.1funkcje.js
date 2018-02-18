7f2e02167aae6f289c6671197f5453424a4fbd36
Michal Rodzen <michalrodzen1994@gmail.com>
2018-02-18 22:40:30 +0100

    Change .js code

--- 9.1funkcje.js
+++ 9.1funkcje.js
@@ -1,19 +1,19 @@
 var a,
     h,
     triangleArea = getTriangleArea(10, 15),
-    triangleArea1 = getTriangleArea(2, 2),
-    triangleArea2 = getTriangleArea(2,-1);
+    triangleAreaSecond = getTriangleArea(2, 2),
+    triangleAreaThird = getTriangleArea(2,-1);
 
 function getTriangleArea (a , h) {
 
     if ((a >= 0) && (h >= 0)) {
         return a*h/2;
     } else {
-        console.log('Nieprawidłowe dane!');
+        return 'Nieprawidłowe dane!';
     }
 }
 console.log(triangleArea);
-console.log(triangleArea1);
-console.log(triangleArea2);
+console.log(triangleAreaSecond);
+console.log(triangleAreaThird);
 
 
